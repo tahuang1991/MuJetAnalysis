bfb01f87aa2df3b07255304e7f273f126e3ff124
Sven Dildick <sven.dildick@cern.ch>
2015-01-20 14:48:32 -0600

    first working version

--- Configuration/python/HLT_TrkMu15_DoubleTrkMu5_v1_cfi.py
+++ Configuration/python/HLT_TrkMu15_DoubleTrkMu5_v1_cfi.py
@@ -1,4 +1,4 @@
-# /dev/CMSSW_7_3_0/GRun/V25 (CMSSW_7_3_0_HLT1)
+## Customization for HLT_TrkMu15_DoubleTrkMu5_v1
 
 import FWCore.ParameterSet.Config as cms
 
@@ -7,7 +7,7 @@ hltL3pfL1sDoubleMu103p5L1f0L2pf0L3PreFiltered5 = cms.EDFilter( "HLTMuonL3PreFilt
     saveTags = cms.bool( True ),
     PreviousCandTag = cms.InputTag( "hltL2pfL1sDoubleMu103p5L1f0L2PreFiltered0" ),
     MinNmuonHits = cms.int32( 0 ),
-    MinN = cms.int32( 2 ),
+    MinN = cms.int32( 3 ),
     MinTrackPt = cms.double( 0.0 ),
     MaxEta = cms.double( 2.5 ),
     MaxDXYBeamSpot = cms.double( 9999.0 ),
@@ -77,51 +77,58 @@ hltSingleTrkMu15Filtered = cms.EDFilter("HLTMuonTrkFilter",
     allowedTypeMask = cms.uint32( 255 )
 )
 
-## custom trigger
-## 2 L1 muons with pt 10-3.5 or 12-5
-## 2 L3 muons with pt 15-5 or 15-7
-## TrkMu:
-##    3 mu with pt>5
-##    1 mu with pt>17
+def add_HLT_TrkMu15_DoubleTrkMu5_v1(process):
 
-"""
-Step 1: Start from HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v1; rename to HLT_TrkMu15_DoubleTrkMu5_v1
-Step 2: Remove the isolation sequence
-Step 3: Add the TrkMu reconstruction sequence
-Step 4: Add TriTrkMu pt5 filter (HLTMuonTrkFilter)
-Step 5: Add SingleTrkMu15 filter (HLTMuonTrkFilter)
-"""
-process.HLT_TrkMu15_DoubleTrkMu5_v1 = cms.Path( 
-    process.HLTBeginSequence + 
-    process.hltL1sL1DoubleMu103p5ORDoubleMu125 + 
-    process.hltPreMu17TrkIsoVVLMu8TrkIsoVVL + 
-    process.hltL1fL1sDoubleMu103p5ORDoubleMu125L1Filtered0 + 
-    process.HLTL2muonrecoSequence + 
-    process.hltL2pfL1sDoubleMu103p5L1f0L2PreFiltered0 + 
-    process.hltL2fL1sDoubleMu103p5L1f0L2Filtered10OneMu + 
-    process.HLTL3muonrecoSequence + 
-    process.hltL3pfL1sDoubleMu103p5L1f0L2pf0L3PreFiltered8 + 
-    process.hltL3fL1sDoubleMu103p5L1f0L2f10OneMuL3Filtered17 + 
-    process.HLTTrackerMuonSequence +
-    process.hltTriTrkMu5Filtered +
-    process.hltSingleTrkMu15Filtered +
-    process.HLTEndSequence 
-)
+    """
+    Step 1: Start from HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v1; rename to HLT_TrkMu15_DoubleTrkMu5_v1
+    Step 2: Remove the isolation sequence
+    Step 3: Add the TrkMu reconstruction sequence
+    Step 4: Add TriTrkMu pt5 filter (HLTMuonTrkFilter)
+    Step 5: Add SingleTrkMu15 filter (HLTMuonTrkFilter)
+    Step 6: Replace the L2 and L3 RECO sequence by variants that do not have dxy cut
+    Step 7: Change L3 prefilter and filter
+    """
+    process.hltL3pfL1sDoubleMu103p5L1f0L2pf0L3PreFiltered5 = hltL3pfL1sDoubleMu103p5L1f0L2pf0L3PreFiltered5
+    process.hltL3fL1sDoubleMu103p5L1f0L2f10OneMuL3Filtered15 = hltL3fL1sDoubleMu103p5L1f0L2f10OneMuL3Filtered15
+    process.hltTriTrkMu5Filtered = hltTriTrkMu5Filtered
+    process.hltSingleTrkMu15Filtered = hltSingleTrkMu15Filtered
 
-process.hltOutputFULL = cms.OutputModule( "PoolOutputModule",
-    fileName = cms.untracked.string( "out_hlt.root" ),
-    fastCloning = cms.untracked.bool( False ),
-    dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string( 'RECO' ),
-        filterName = cms.untracked.string( '' )
-    ),
-    SelectEvents = cms.untracked.PSet(  
-        SelectEvents = cms.vstring('*') 
-    ),
-    outputCommands = cms.untracked.vstring( 'keep *' )
-)
-process.FULLOutput = cms.EndPath( process.hltOutputFULL )
+    process.HLT_TrkMu15_DoubleTrkMu5_v1 = cms.Path( 
+        process.HLTBeginSequence + 
+        process.hltL1sL1DoubleMu103p5ORDoubleMu125 + 
+        process.hltPreMu17TrkIsoVVLMu8TrkIsoVVL + 
+        process.hltL1fL1sDoubleMu103p5ORDoubleMu125L1Filtered0 + 
+        process.HLTL2muonrecoSequenceNoVtx + 
+        process.hltL2pfL1sDoubleMu103p5L1f0L2PreFiltered0 + 
+        process.hltL2fL1sDoubleMu103p5L1f0L2Filtered10OneMu + 
+        process.HLTL3NoFiltersNoVtxmuonrecoSequence + 
+        process.hltL3pfL1sDoubleMu103p5L1f0L2pf0L3PreFiltered5 + 
+        process.hltL3fL1sDoubleMu103p5L1f0L2f10OneMuL3Filtered15 + 
+        process.HLTTrackerMuonSequence +
+        process.hltTriTrkMu5Filtered +
+        process.hltSingleTrkMu15Filtered +
+        process.HLTEndSequence 
+    )
+    return process
 
+def addFullOutputModule(process):
+    process.hltOutputFULL = cms.OutputModule( "PoolOutputModule",
+        fileName = cms.untracked.string( "out_hlt.root" ),
+        fastCloning = cms.untracked.bool( False ),
+        dataset = cms.untracked.PSet(
+            dataTier = cms.untracked.string( 'RECO' ),
+            filterName = cms.untracked.string( '' )
+        ),
+        SelectEvents = cms.untracked.PSet(  
+            SelectEvents = cms.vstring('*') 
+        ),
+        outputCommands = cms.untracked.vstring( 'keep *' )
+    )
+    process.FULLOutput = cms.EndPath( process.hltOutputFULL )
+    return process
 
-## Change the seed L1_TripleMu_5_5_3 to L1_TripleMu_5_5_3_HighQ in hltL1sL1TripleMu553
+def changeL1SeedhltL1sL1TripleMu553(process):
+    ## Change the seed L1_TripleMu_5_5_3 to L1_TripleMu_5_5_3_HighQ in hltL1sL1TripleMu553
+    process.hltL1sL1TripleMu553.L1SeedsLogicalExpression = "L1_TripleMu_5_5_3_HighQ"
+    return process
 

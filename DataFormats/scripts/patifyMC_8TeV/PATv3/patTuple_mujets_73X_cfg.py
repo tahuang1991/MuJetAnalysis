26d1ed7e11058866995e5452524908549e2e82f6
Sven Dildick <sven.dildick@cern.ch>
2015-01-20 01:44:04 -0600

    clean-up

--- DataFormats/scripts/patifyMC_8TeV/PATv3/patTuple_mujets_73X_cfg.py
+++ DataFormats/scripts/patifyMC_8TeV/PATv3/patTuple_mujets_73X_cfg.py
@@ -1,11 +1,8 @@
 ## import skeleton process
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
+
 # verbose flags for the PF2PAT modules
 process.options.allowUnscheduled = cms.untracked.bool(True)
-#process.Tracer = cms.Service("Tracer")
-
-#process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
-#process.load("PhysicsTools.PatAlgos.selectionLayer1.selectedPatCandidates_cff")
 
 ### Add MuJet Dataformats
 process.load("MuJetAnalysis.DataFormats.RECOtoPAT_cff")
@@ -16,31 +13,12 @@ process.load("MuJetAnalysis.DataFormats.EventContent_version9_cff")
 process.out.fileName = cms.untracked.string("out_pat.root")
 process.out.outputCommands = process.patOutput.outputCommands
 
-
-
-## ------------------------------------------------------
-#  In addition you usually want to change the following
-#  parameters:
-## ------------------------------------------------------
-#
-#   process.GlobalTag.globaltag =  ...    ##  (according to https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions)
-#                                         ##
+process.maxEvents.input = 100
 process.source = cms.Source("PoolSource",
   fileNames = cms.untracked.vstring(
-    "file:/eos/uscms/store/user/lpcgem/dildick/CMSSW_73/dildick/DarkSUSY_mH_125_mGammaD_0400_ctauExp_2_8TeV_madgraph452_bridge224_LHE_pythia6_RAW/DarkSUSY_mH_125_mGammaD_0400_ctauExp_2_8TeV_madgraph452_bridge224_LHE_pythia6_RECO/ee2b69195f87ad6497ae607e47718adf/out_reco_1_1_XNE.root"
+    #"file:/eos/uscms/store/user/lpcgem/dildick/CMSSW_73/dildick/DarkSUSY_mH_125_mGammaD_0400_ctauExp_2_8TeV_madgraph452_bridge224_LHE_pythia6_RAW/DarkSUSY_mH_125_mGammaD_0400_ctauExp_2_8TeV_madgraph452_bridge224_LHE_pythia6_RECO/ee2b69195f87ad6497ae607e47718adf/out_reco_1_1_XNE.root"
 #      "file:CrabJobs/out_reco.root"
+    "file:/eos/uscms/store/user/lpcgem/dildick/CMSSW_73/dildick/DarkSUSY_mH_125_mGammaD_0400_ctauExp_2_8TeV_madgraph452_bridge224_LHE_pythia6_RAW/DarkSUSY_mH_125_mGammaD_0400_ctauExp_2_8TeV_madgraph452_bridge224_LHE_pythia6_HLT_4/3f3291e696aa213db7125f5241d82f87/out_hlt_1_1_lpI.root"
   )
 )
 
-#                                         ##
-process.maxEvents.input = 1000
-#                                         ##
-#   process.out.outputCommands = [ ... ]  ##  (e.g. taken from PhysicsTools/PatAlgos/python/patEventContent_cff.py)
-#                                         ##
-#process.out.fileName = 'patTuple_PF2PAT.root'
-#                                         ##
-#   process.options.wantSummary = False   ##  (to suppress the long output at the end of the job)
-
-#prints modules as they are called with times
-#from Validation.Performance.TimeMemoryInfo import customise
-#process = customise(process)

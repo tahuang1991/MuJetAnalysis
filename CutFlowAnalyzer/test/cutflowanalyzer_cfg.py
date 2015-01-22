e63530ea268f49fb6dce0d85e1e45882426d740d
Sven Dildick <sven.dildick@cern.ch>
2015-01-22 14:07:04 -0600

    add more hlt paths

--- CutFlowAnalyzer/test/cutflowanalyzer_cfg.py
+++ CutFlowAnalyzer/test/cutflowanalyzer_cfg.py
@@ -62,15 +62,18 @@ process.cutFlowAnalyzer = cms.EDAnalyzer('CutFlowAnalyzer',
     DiMuons_Iso_Max = cms.double(2.0),
     nThrowsConsistentVertexesCalculator = cms.int32(100000),
     hltPaths = cms.vstring(
+        'HLT_DoubleMu33NoFiltersNoVtx_v1',
         'HLT_Mu17_Mu8_DZ_v1',
-        'HLT_Mu17_Mu8_DZ_v1_NoDz',
-        'HLT_Mu30_TkMu11_v1',
         'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v1',
-        'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v1_NoIso',
         'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v1',
-        'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v1_NoIso',
+        'HLT_Mu30_TkMu11_v1',
+        'HLT_Mu38NoFiltersNoVtx_Photon38_CaloIdL_v1',
         'HLT_TripleMu_12_10_5_v1',
-        'HLT_TrkMu15_DoubleTrkMu5_v1'
+        'HLT_TripleMu_12_10_5_onlyL1NewSeed_v1',
+        'HLT_TrkMu15_DoubleTrkMu5_v1',
+        'HLT_Mu17_Mu8_DZ_v1_NoDz',
+        'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v1_NoIso',
+        'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v1_NoIso'
     )
 )
 

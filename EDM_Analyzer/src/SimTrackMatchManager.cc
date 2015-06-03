4690262c1507127e6415e9215af45292137bdb2f
Jose <jrdv009@tamu.edu>
2015-06-03 14:46:40 -0500

    EDM_Test_C

---
+++ EDM_Analyzer/src/SimTrackMatchManager.cc
@@ -0,0 +1,10 @@
+#include "MuJetAnalysis/EDM_Analyzer/src/SimTrackMatchManager.h"
+
+SimTrackMatchManager::SimTrackMatchManager(const SimTrack& t, const SimVertex& v,
+      const edm::ParameterSet& ps, const edm::Event& ev, const edm::EventSetup& es)
+: simhits_(t, v, ps, ev, es)
+{
+}
+
+SimTrackMatchManager::~SimTrackMatchManager() {
+}

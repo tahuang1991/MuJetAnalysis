ac4aa58ae70709db570d5cd925f6977d0aae27c3
Sven Dildick <sven.dildick@cern.ch>
2015-06-05 15:05:21 -0500

    rename

--- HLTBendingAngle/test/runHLTBendingAngle_cfg.py
+++ HLTBendingAngle/test/runHLTBendingAngle_cfg.py
@@ -1,54 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("demo")
+process = cms.Process("HLTBending")
 
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
-
-process.load('Configuration.StandardSequences.Services_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('Configuration.StandardSequences.Services_cff')
-process.load("MuJetAnalysis.HLTBendingAngle.CfiFile_cfi")
 process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi')
 process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi')
+process.load("MuJetAnalysis.HLTBendingAngle.HLTBendingAngle_cfi")
 
-#from Geometry.CMSCommonData.cmsExtendedGeometry2023XML_cfi import *
-#from Geometry.TrackerNumberingBuilder.trackerTopology2023Constants_cfi import *
-#from Configuration.Geometry.GeometryExtended2023Muon_cff import *
-process.load("Geometry.CMSCommonData.cmsExtendedGeometry2023XML_cfi")
-process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
-process.load("Geometry.DTGeometryBuilder.dtGeometry_cfi")
-
-
-
-
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1)
-)
-
-
-#process.options = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound'))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100))
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
    fileNames = cms.untracked.vstring(
       'file:outputA.root'
  )
 )
-
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string("out_ana.root"),
 	closeFileFast = cms.untracked.bool(True)
 )
 
-
-
-
-
-process.p = cms.Path(process.demo)
+process.p = cms.Path(process.HLTBendingAngle)
 
 
 

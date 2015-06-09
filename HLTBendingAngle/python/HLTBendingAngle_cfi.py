7fc98b7b594a6cc0bd549fe991ee921b80024701
Sven Dildick <sven.dildick@cern.ch>
2015-06-09 00:32:55 -0500

    bugfix

--- HLTBendingAngle/python/HLTBendingAngle_cfi.py
+++ HLTBendingAngle/python/HLTBendingAngle_cfi.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
-HLTBendingAngle = cms.EDAnalyzer('HLTBendingAngle'
+
+HLTBendingAngle = cms.EDAnalyzer('HLTBendingAngle',
     verbose = cms.untracked.int32(0),
     simTrackMatching = SimTrackMatching
 )

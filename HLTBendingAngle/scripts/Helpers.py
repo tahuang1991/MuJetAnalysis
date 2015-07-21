3bc9c399285bf334a2511c8ef2d218062f0cb4b5
Sven Dildick <sven.dildick@cern.ch>
2015-07-21 14:19:04 -0500

    Create Helpers.py

---
+++ HLTBendingAngle/scripts/Helpers.py
@@ -0,0 +1,74 @@
+from ROOT import *
+import math
+import array
+from math import log10, floor
+
+#_______________________________________________________________________________
+def get_1D(p, title, h_name, h_bins, to_draw, cut, opt = "", color = kBlue):
+    gStyle.SetStatStyle(0)
+    gStyle.SetOptStat(11111111)
+    #nbins = len(xbins)
+    #h = TH1F("h_name", "h_name", nbins, xbins);
+    p.tree.Draw(to_draw + ">>" + h_name + h_bins, cut)
+    h = TH1F(gDirectory.Get(h_name).Clone(h_name))
+    if not h:
+        sys.exit('%s does not exist'%(to_draw))
+    h.SetTitle(title)
+    h.SetLineWidth(2)
+    h.SetLineColor(color)    
+    h.SetMinimum(0.)
+    SetOwnership(h, False)
+    return h
+
+#_______________________________________________________________________________
+def AND(*arg):
+    """AND of any number of TCuts in PyROOT"""
+    length = len(arg)
+    if length == 0:
+        print "ERROR: invalid number of arguments"
+        return
+    if length == 1:
+        return arg[0] 
+    if length==2:
+        return ANDtwo(arg[0],arg[1])
+    if length>2:
+        result = arg[0]
+        for i in range(1,len(arg)):
+            result = ANDtwo(result,arg[i])
+        return result
+
+#_______________________________________________________________________________
+def OR(*arg):
+    """OR of any number of TCuts in PyROOT"""
+    length = len(arg)
+    if length == 0:
+        print "ERROR: invalid number of arguments"
+        return
+    if length == 1:
+        return arg[0] 
+    if length==2:
+        return ORtwo(arg[0],arg[1])
+    if length>2:
+        result = arg[0]
+        for i in range(1,len(arg)):
+            result = ORtwo(result,arg[i])
+        return result
+
+#_______________________________________________________________________________
+def ANDtwo(cut1,cut2):
+    """AND of two TCuts in PyROOT"""
+    if cut1.GetTitle() == "":
+        return cut2
+    if cut2.GetTitle() == "":
+        return cut1
+    return TCut("(%s) && (%s)"%(cut1.GetTitle(),cut2.GetTitle()))
+
+
+#_______________________________________________________________________________
+def ORtwo(cut1,cut2):
+    """OR of two TCuts in PyROOT"""
+    if cut1.GetTitle() == "":
+        return cut2
+    if cut2.GetTitle() == "":
+        return cut1
+    return TCut("(%s) || (%s)"%(cut1.GetTitle(),cut2.GetTitle()))
